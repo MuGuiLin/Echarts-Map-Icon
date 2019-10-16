6e46f26bd403d014c2582bdecbd14e0a3ab8767a
muguilin <muguilin@foxmail.com>
2019-10-16 18:43:45 +0800

    基础版本

---
+++ js/zunyi-data.js
@@ -0,0 +1,137 @@
+const zunyiData = [
+    {
+        "adcode": "520321",
+        "people_count_2010": 942904,
+        "lat": 27.535288,
+        "lng": 106.831668,
+        "name": "遵义县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520382",
+        "people_count_2010": 546477,
+        "lat": 27.803377,
+        "lng": 106.412476,
+        "name": "仁怀市",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520324",
+        "people_count_2010": 389634,
+        "lat": 28.550337,
+        "lng": 107.441872,
+        "name": "正安县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520330",
+        "people_count_2010": 523180,
+        "lat": 28.327826,
+        "lng": 106.200954,
+        "name": "习水县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520302",
+        "people_count_2010": 656592,
+        "lat": 27.694395,
+        "lng": 106.943784,
+        "name": "红花岗区",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520323",
+        "people_count_2010": 380083,
+        "lat": 27.951342,
+        "lng": 107.191024,
+        "name": "绥阳县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520325",
+        "people_count_2010": 244159,
+        "lat": 28.880088,
+        "lng": 107.605342,
+        "name": "道真仡佬族苗族自治县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520322",
+        "people_count_2010": 521840,
+        "lat": 28.131559,
+        "lng": 106.826591,
+        "name": "桐梓县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520327",
+        "people_count_2010": 313128,
+        "lat": 27.960858,
+        "lng": 107.722021,
+        "name": "凤冈县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520303",
+        "people_count_2010": 438279,
+        "lat": 27.706626,
+        "lng": 106.937265,
+        "name": "汇川区",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520328",
+        "people_count_2010": 377358,
+        "lat": 27.765839,
+        "lng": 107.485723,
+        "name": "湄潭县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520381",
+        "people_count_2010": 237052,
+        "lat": 28.587057,
+        "lng": 105.698116,
+        "name": "赤水市",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520326",
+        "people_count_2010": 321657,
+        "lat": 28.521567,
+        "lng": 107.887857,
+        "name": "务川仡佬族苗族自治县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520329",
+        "people_count_2010": 234739,
+        "lat": 27.221552,
+        "lng": 107.892566,
+        "name": "余庆县",
+        "level": "district",
+        "parent": "遵义市"
+    },
+    {
+        "adcode": "520304",
+        "people_count_2010": 520304,
+        "lat": 27.535288,
+        "lng": 106.831668,
+        "name": "播州区",
+        "level": "district",
+        "parent": "遵义市"
+    }
+]
